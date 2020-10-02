f693654441be9720ed33b640d59c86535e18e82a
Shanmukh Karra <shanmukh1608@gmail.com>
2020-10-02 17:49:32 +0300

    Index: Implemented parsing assuming functions exist

--- src/executors/index.cpp
+++ src/executors/index.cpp
@@ -7,7 +7,7 @@
 bool syntacticParseINDEX()
 {
     logger.log("syntacticParseINDEX");
-    if (tokenizedQuery.size() != 7 || tokenizedQuery[1] != "ON" || tokenizedQuery[3] != "FROM" || tokenizedQuery[5] != "USING")
+    if ((tokenizedQuery.size() != 7 && tokenizedQuery.size() != 9) || tokenizedQuery[1] != "ON" || tokenizedQuery[3] != "FROM" || tokenizedQuery[5] != "USING")
     {
         cout << "SYNTAX ERROR" << endl;
         return false;
@@ -16,10 +16,25 @@ bool syntacticParseINDEX()
     parsedQuery.indexColumnName = tokenizedQuery[2];
     parsedQuery.indexRelationName = tokenizedQuery[4];
     string indexingStrategy = tokenizedQuery[6];
+
     if (indexingStrategy == "BTREE")
+    {
         parsedQuery.indexingStrategy = BTREE;
+        if (tokenizedQuery[7] != "FANOUT")
+        {
+            cout << "SYNTAX ERROR" << endl;
+            return false;
+        }
+    }
     else if (indexingStrategy == "HASH")
+    {
         parsedQuery.indexingStrategy = HASH;
+        if (tokenizedQuery[7] != "BUCKETS")
+        {
+            cout << "SYNTAX ERROR" << endl;
+            return false;
+        }
+    }
     else if (indexingStrategy == "NOTHING")
         parsedQuery.indexingStrategy = NOTHING;
     else
@@ -44,7 +59,7 @@ bool semanticParseINDEX()
         return false;
     }
     Table* table = tableCatalogue.getTable(parsedQuery.indexRelationName);
-    if(table->indexed){
+    if(table->indexed && parsedQuery.indexingStrategy != NOTHING){
         cout << "SEMANTIC ERROR: Table already indexed" << endl;
         return false;
     }
@@ -53,6 +68,25 @@ bool semanticParseINDEX()
 
 void executeINDEX()
 {
+    Table* table = tableCatalogue.getTable(parsedQuery.indexRelationName);
+    table->indexingStrategy = parsedQuery.indexingStrategy;
+    cout<<"Table = "<<table->tableName<<endl<<"Strategy = "<<table->indexingStrategy<<endl;
+    if (table->indexingStrategy == NOTHING)
+    {
+        table->indexed = false;
+        table->indexedColumn = "";
+        clearIndex();
+    }
+    else
+    {
+        table->indexed=true;
+        table->indexedColumn = parsedQuery.indexColumnName;
+        if (table->indexingStrategy == HASH)
+            linearHash(atoi(tokenizedQuery[8].c_str()));
+        else if (table->indexingStrategy == BTREE)
+            bPlusTree(atoi(tokenizedQuery[8].c_str()));
+    }
+    cout<<"Column = "<<table->indexedColumn<<endl<<"Indexed = "<<table->indexed<<endl;
     logger.log("executeINDEX");
     return;
 }
