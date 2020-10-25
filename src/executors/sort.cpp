0ce8f8c4a5f6392305cde2208b217f411f5a1321
Shanmukh Karra <shanmukh1608@gmail.com>
2020-10-25 13:40:48 +0300

    External sort: Creating runs, sorting, and writing back (merge phase pending)

--- src/executors/sort.cpp
+++ src/executors/sort.cpp
@@ -9,18 +9,21 @@
  */
 bool syntacticParseSORT(){
     logger.log("syntacticParseSORT");
-    if(tokenizedQuery.size()!= 8 || tokenizedQuery[4] != "BY" || tokenizedQuery[6] != "IN"){
+    if((tokenizedQuery.size() != 8 && tokenizedQuery.size() != 10) || tokenizedQuery[4] != "BY" || tokenizedQuery[6] != "IN" || (tokenizedQuery.size() == 10 && tokenizedQuery[8] != "BUFFER")){
         cout<<"SYNTAX ERROR"<<endl;
         return false;
     }
     parsedQuery.queryType = SORT;
     parsedQuery.sortResultRelationName = tokenizedQuery[0];
-    parsedQuery.sortColumnName = tokenizedQuery[3];
-    parsedQuery.sortRelationName = tokenizedQuery[5];
-    string sortingStrateg = tokenizedQuery[7];
-    if(sortingStrateg == "ASC")
+    parsedQuery.sortRelationName = tokenizedQuery[3];
+    parsedQuery.sortColumnName = tokenizedQuery[5];
+    if (tokenizedQuery.size() == 10)
+        parsedQuery.sortBufferSize = stoi(tokenizedQuery[9]);
+
+    string sortingStrategy = tokenizedQuery[7];
+    if(sortingStrategy == "ASC")
         parsedQuery.sortingStrategy = ASC;
-    else if(sortingStrateg == "DESC")
+    else if(sortingStrategy == "DESC")
         parsedQuery.sortingStrategy = DESC;
     else{
         cout<<"SYNTAX ERROR"<<endl;
@@ -50,7 +53,57 @@ bool semanticParseSORT(){
     return true;
 }
 
-void executeSORT(){
-    logger.log("executeSORT");
+void executeSORT()
+{
+    Table table = *tableCatalogue.getTable(parsedQuery.sortRelationName);
+
+    int runSize = table.maxRowsPerBlock * parsedQuery.sortBufferSize;
+    int numberOfRuns = ceil((float)table.rowCount / (float)runSize);
+
+    Cursor cursor = table.getCursor();
+    vector<int> row = cursor.getNext();
+    int columnIndex = table.getColumnIndex(parsedQuery.sortColumnName);
+    int runCount = 0;
+    while (!row.empty())
+    {
+        vector<vector<int>> rows;
+        int rowsRead = 0;
+        while (!row.empty() && rowsRead != runSize)
+        {
+            rows.push_back(row);
+            row = cursor.getNext();
+            rowsRead++;
+        }
+
+        if (rows.size())
+            sort(rows.begin(), rows.end(),
+                [&](const std::vector<int> &a, const std::vector<int> &b) {
+                    if (parsedQuery.sortingStrategy == ASC)
+                        return a[columnIndex] < b[columnIndex];
+                    else if (parsedQuery.sortingStrategy == DESC)
+                        return a[columnIndex] >= b[columnIndex];
+                });
+
+        int rowsWritten = 0;
+        int blocksWritten = 0;
+        while (rowsWritten < rowsRead)
+        {
+            vector<vector<int>> subvector;
+            if (rowsWritten + table.maxRowsPerBlock <= rowsRead)
+            {
+                subvector = {rows.begin() + rowsWritten, rows.begin() + rowsWritten + table.maxRowsPerBlock};
+                rowsWritten += table.maxRowsPerBlock;
+            }
+            else
+            {
+                subvector = {rows.begin() + rowsWritten, rows.end()};
+                rowsWritten = rowsRead;
+            }
+            
+            bufferManager.writeTablePage(table.tableName + "_Run_" + to_string(runCount), blocksWritten++, subvector, subvector.size());
+        }
+        runCount++;
+    }
+
     return;
-}
+}   
