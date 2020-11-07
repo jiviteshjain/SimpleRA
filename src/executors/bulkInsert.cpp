88532e41f554448651dfa6d44d0d069d6c0ea607
Shanmukh Karra <shanmukh1608@gmail.com>
2020-11-07 12:45:23 +0300

    Bulk insert: Implemented for non-indexed and linearly hashed tables

--- src/executors/bulkInsert.cpp
+++ src/executors/bulkInsert.cpp
@@ -58,4 +58,93 @@ bool semanticParseBULKINSERT()
 void executeBULKINSERT()
 {
     logger.log("executeBULKINSERT");
+    Table *table = tableCatalogue.getTable(parsedQuery.bulkInsertRelationName);
+
+    ifstream fin(parsedQuery.bulkInsertFileName, ios::in);
+    string line, word;
+    vector<int> row(table->columnCount, 0);
+
+    getline(fin, line);
+
+    if (table->indexed == false)
+    {
+        vector<vector<int>> rowsInPage(table->maxRowsPerBlock, row);
+        int pageCounter = 0;
+        while (getline(fin, line))
+        {
+            stringstream s(line);
+            for (int columnCounter = 0; columnCounter < table->columnCount; columnCounter++)
+            {
+                if (!getline(s, word, ','))
+                    return;
+                row[columnCounter] = stoi(word);
+                rowsInPage[pageCounter][columnCounter] = row[columnCounter];
+            }
+            pageCounter++;
+            table->updateStatistics(row);
+            if (pageCounter == table->maxRowsPerBlock - table->rowsPerBlockCount[table->blockCount - 1])
+            {
+                vector<vector<int>> rowsInLastPage = bufferManager.getTablePage(table->tableName, table->blockCount - 1).data;
+                rowsInLastPage.resize(table->rowsPerBlockCount[table->blockCount - 1]);
+                rowsInLastPage.insert(std::end(rowsInLastPage), std::begin(rowsInPage), std::end(rowsInPage));
+                bufferManager.writeTablePage(table->tableName, table->blockCount - 1, rowsInLastPage, table->maxRowsPerBlock);
+                table->rowsPerBlockCount[table->blockCount - 1] = table->maxRowsPerBlock;
+                pageCounter = 0;
+                break;
+            }
+        }
+
+        while (getline(fin, line))
+        {
+            stringstream s(line);
+            for (int columnCounter = 0; columnCounter < table->columnCount; columnCounter++)
+            {
+                if (!getline(s, word, ','))
+                    return;
+                row[columnCounter] = stoi(word);
+                rowsInPage[pageCounter][columnCounter] = row[columnCounter];
+            }
+            pageCounter++;
+            table->updateStatistics(row);
+            if (pageCounter == table->maxRowsPerBlock)
+            {
+                bufferManager.writeTablePage(table->tableName, table->blockCount, rowsInPage, pageCounter);
+                table->blockCount++;
+                table->rowsPerBlockCount.emplace_back(pageCounter);
+                pageCounter = 0;
+            }
+        }
+
+        if (pageCounter <= table->maxRowsPerBlock - table->rowsPerBlockCount[table->blockCount - 1])
+        {
+            vector<vector<int>> rowsInLastPage = bufferManager.getTablePage(table->tableName, table->blockCount - 1).data;
+            rowsInLastPage.resize(table->rowsPerBlockCount[table->blockCount - 1]);
+            rowsInLastPage.insert(std::end(rowsInLastPage), std::begin(rowsInPage), std::end(rowsInPage));
+            bufferManager.writeTablePage(table->tableName, table->blockCount - 1, rowsInLastPage, table->rowsPerBlockCount[table->blockCount - 1] + pageCounter);
+            table->rowsPerBlockCount[table->blockCount - 1] = table->maxRowsPerBlock;
+            pageCounter = 0;
+        }
+        else if (pageCounter < table->maxRowsPerBlock)
+        {
+            bufferManager.writeTablePage(table->tableName, table->blockCount, rowsInPage, pageCounter);
+            table->blockCount++;
+            table->rowsPerBlockCount.emplace_back(pageCounter);
+            pageCounter = 0;
+        }
+    }
+    else if (table->indexingStrategy == HASH)
+    {
+        while (getline(fin, line))
+        {
+            stringstream s(line);
+            for (int columnCounter = 0; columnCounter < table->columnCount; columnCounter++)
+            {
+                if (!getline(s, word, ','))
+                    return;
+                row[columnCounter] = stoi(word);
+            }
+
+            table->insert(row);
+        }
+    }
 }
