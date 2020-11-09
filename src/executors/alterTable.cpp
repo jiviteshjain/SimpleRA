8a9410e854c82f533e5402ef08857a02a874aea4
Shanmukh Karra <shanmukh1608@gmail.com>
2020-11-09 17:02:13 +0300

    Alter table: Implemented for non-indexed and linearly hashed tables

--- src/executors/alterTable.cpp
+++ src/executors/alterTable.cpp
@@ -28,15 +28,17 @@ bool semanticParseALTERTABLE()
         return false;
     }
 
+    Table *table = tableCatalogue.getTable(parsedQuery.alterTableRelationName);
+
     if (parsedQuery.alterTableOperationName == "ADD")
-        if (tableCatalogue.getTable(parsedQuery.alterTableRelationName)->isColumn(parsedQuery.alterTableColumnName))
+        if (table->isColumn(parsedQuery.alterTableColumnName))
         {
             cout << "SEMANTIC ERROR: Column already exists in relation";
             return false;
         }
 
     if (parsedQuery.alterTableOperationName == "DELETE")
-        if (!tableCatalogue.getTable(parsedQuery.alterTableRelationName)->isColumn(parsedQuery.alterTableColumnName))
+        if (!table->isColumn(parsedQuery.alterTableColumnName))
         {
             cout << "SEMANTIC ERROR: Column doesn't exist in relation";
             return false;
@@ -47,12 +49,143 @@ bool semanticParseALTERTABLE()
 void executeALTERTABLE()
 {
     logger.log("executeALTERTABLE");
+    Table *table = tableCatalogue.getTable(parsedQuery.alterTableRelationName);
 
-    // Table *table = new Table(parsedQuery.loadRelationName);
-    // if (table->load())
-    // {
-    //     tableCatalogue.insertTable(table);
-    //     cout << "Loaded Table. Column Count: " << table->columnCount << " Row Count: " << table->rowCount << endl;
-    // }
+    vector<string> columns = table->columns;
+    int columnIndex;
+    if (parsedQuery.alterTableOperationName == "ADD")
+        columns.push_back(parsedQuery.alterTableColumnName);
+    else if (parsedQuery.alterTableOperationName == "DELETE")
+    {
+        columnIndex = table->getColumnIndex(parsedQuery.alterTableColumnName);
+        columns.erase(columns.begin() + columnIndex);
+        if (columns.size() == 0)
+        {
+            tableCatalogue.deleteTable(table->tableName);
+            return;
+        }
+    }
+
+    Table *newTable = new Table(parsedQuery.alterTableRelationName + "_altertemp", columns);
+
+    if (!table->indexed || (table->indexingStrategy == HASH && table->indexedColumn == columnIndex))
+    {
+        vector<int> row;
+        Cursor cursor;
+
+        if (!table->indexed)
+        {
+            cursor = table->getCursor();
+            row = cursor.getNext();
+        }
+        else if (table->indexingStrategy == HASH)
+        {
+            cursor = table->getCursor(0, 0);
+            row = cursor.getNextInAllBuckets();
+        }
+
+        vector<vector<int>> rows;
+        while (!row.empty())
+        {
+            if (parsedQuery.alterTableOperationName == "ADD")
+                row.push_back(0);
+            else if (parsedQuery.alterTableOperationName == "DELETE")
+                row.erase(row.begin() + columnIndex);
+
+            newTable->updateStatistics(row);
+
+            rows.push_back(row);
+            if (rows.size() == newTable->maxRowsPerBlock)
+            {
+                newTable->rowsPerBlockCount.emplace_back(rows.size());
+                bufferManager.writeTablePage(newTable->tableName, newTable->blockCount, rows, rows.size());
+                newTable->blockCount++;
+                rows.clear();
+            }
+
+            if (!table->indexed)
+                row = cursor.getNext();
+            else if (table->indexingStrategy == HASH)
+                row = cursor.getNextInAllBuckets();
+        }
+
+        if (rows.size())
+        {
+            newTable->rowsPerBlockCount.emplace_back(rows.size());
+            bufferManager.writeTablePage(newTable->tableName, newTable->blockCount, rows, rows.size());
+            newTable->blockCount++;
+            rows.clear();
+        }
+    }
+
+    else if (table->indexingStrategy == HASH)
+    {
+        newTable->indexed = true;
+        newTable->indexingStrategy = HASH;
+        newTable->M = table->M;
+        newTable->N = table->N;
+        newTable->blocksInBuckets = vector<vector<int>>(table->blocksInBuckets.size());
+        for (int bucket = 0; bucket < table->blocksInBuckets.size(); bucket++)
+        {
+            if (table->blocksInBuckets[bucket].size())
+            {
+                Cursor cursor = table->getCursor(bucket, 0);
+                vector<int> row = cursor.getNextInBucket();
+                vector<vector<int>> rows;
+                int chainCount = 0;
+
+                while (!row.empty())
+                {
+                    if (parsedQuery.alterTableOperationName == "ADD")
+                        row.push_back(0);
+                    else if (parsedQuery.alterTableOperationName == "DELETE")
+                        row.erase(row.begin() + columnIndex);
+
+                    newTable->updateStatistics(row);
+                    rows.push_back(row);
+                    if (rows.size() == newTable->maxRowsPerBlock)
+                    {
+                        newTable->blocksInBuckets[bucket].emplace_back(rows.size());
+                        bufferManager.writeHashPage(newTable->tableName, bucket, chainCount++, rows);
+                        rows.clear();
+                    }
+
+                    row = cursor.getNextInBucket();
+                }
+
+                if (rows.size())
+                {
+                    newTable->blocksInBuckets[bucket].emplace_back(rows.size());
+                    bufferManager.writeHashPage(newTable->tableName, bucket, chainCount++, rows);
+                    rows.clear();
+                }
+            }
+        }
+    }
+
+    if (newTable->rowCount)
+    {
+        newTable->sourceFileName = table->sourceFileName;
+        string tableName = table->tableName;
+        tableCatalogue.deleteTable(table->tableName);
+        vector<vector<int>> rows;
+        for (int i = 0; i < newTable->blockCount; i++)
+        {
+            rows = bufferManager.getTablePage(newTable->tableName, i).data;
+            bufferManager.deleteTableFile(newTable->tableName, i);
+            rows.resize(newTable->rowsPerBlockCount[i]);
+            bufferManager.writeTablePage(tableName, i, rows, rows.size());
+        }
+
+        newTable->tableName = tableName;
+
+        tableCatalogue.insertTable(newTable);
+    }
+    else
+    {
+        cout << "Empty Table" << endl;
+        newTable->unload();
+        delete newTable;
+    }
     return;
 }
